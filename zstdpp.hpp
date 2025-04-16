32c53ba55a389212afae6e63c6f44a3b62292312
Andy <168050041+garcia-andy@users.noreply.github.com>
2025-04-15 21:18:37 -0400

    Update with two new functions for work with buffers of bytes
    Added compress_buffer & decompress_buffer for compression/decompression from vector of unsigned char (uint8_t).
    
    Extra: Added aliases for byte_t = uint8_t & buffer_t = vector<byte_t>.

--- zstdpp.hpp
+++ zstdpp.hpp
@@ -1,9 +1,42 @@
-#include <string.h>
-
+#include <vector>
+#include <cstdint>
+#include <string>
 #include "zstd.h"
 
-
 namespace zstdpp {
+
+using byte_t = std::uint8_t;
+using buffer_t = std::vector<byte_t>;
+
+inline buffer_t compress_buffer( buffer_t const& data, std::uint8_t compress_level = 3 ){
+    size_t est_compress_size = ZSTD_compressBound(data.size());
+    buffer_t comp_buffer{};
+    comp_buffer.resize(est_compress_size);
+    
+    auto compress_size =
+        ZSTD_compress((void*)comp_buffer.data(), est_compress_size, data.data(),
+                      data.size(), compress_level);
+        
+    comp_buffer.resize(compress_size);
+    comp_buffer.shrink_to_fit();
+    return comp_buffer;
+}
+
+inline buffer_t decompress_buffer(buffer_t data) {
+
+  auto const est_decomp_size =
+      ZSTD_getFrameContentSize(data.data(), data.size());
+
+  buffer_t decomp_buffer{};
+  decomp_buffer.resize(est_decomp_size);
+
+  size_t const decomp_size = ZSTD_decompress(
+      (void*)decomp_buffer.data(), est_decomp_size, data.data(), data.size());
+
+  decomp_buffer.resize(decomp_size);
+  decomp_buffer.shrink_to_fit();
+  return decomp_buffer;
+}
 
 inline std::string compress(const std::string data, int compress_level) {
 
