e91e07efad22f8b54ca50643c1fce6cc9dc58a5b
garcia-andy <garciandy579@gmail.com>
2025-04-18 01:13:04 -0400

    Added fs namespace with utilities functions for filesystems works

--- tests/helpers.hpp
+++ tests/helpers.hpp
@@ -9,6 +9,41 @@
 using byte_t = zstdpp::byte_t;
 using buffer_t = zstdpp::buffer_t;
 
+namespace fs{
+
+    inline void create_file(std::string const& filename, buffer_t const& buffer){
+        std::ofstream file(filename, std::ios::binary);
+        file.write((char*)buffer.data(), buffer.size());
+    }
+
+    inline size_t get_file_size(std::ifstream& file){
+        file.seekg(0, std::ios::end);
+        auto const file_size = file.tellg();
+        file.seekg(0, std::ios::beg);
+        return file_size;
+    }
+    
+    inline size_t read_file_on(const std::string filename, buffer_t& buffer){
+        std::ifstream file(filename, std::ios::binary);
+        auto const file_size = get_file_size(file);
+        buffer.resize(file_size);
+        file.read((char*)buffer.data(), file_size);
+        return file_size;
+    }
+    
+    inline buffer_t read_file(std::string const& filename){
+        buffer_t buffer{};
+        read_file_on(filename,buffer);
+        return buffer;
+    }
+    
+    inline void write_file(std::string const& filename, buffer_t const& buffer){
+        std::ofstream file(filename, std::ios::binary);
+        file.write((char*)buffer.data(), buffer.size());
+    }
+    
+} // namespace fs
+
 inline double ratio(std::size_t compressed_size, std::size_t decompressed_size){
     return ((double)compressed_size / (double)decompressed_size) * 100.0;
 }
